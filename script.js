9cf5312634989590627559e925ff5330e6cfa1c0
Geoffrey Datema <datemag@algonquincollege.com>
2020-01-30 16:14:42 -0500

    Adds object feature

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-let variable = 10
+let variable = {
+    property1: 123,
+    property2: 'Steve'
+}
 
-console.log(`The variable is ${variable}`)
+console.log(`The variable is ${variable.property1}, ${variable.property2}`)
